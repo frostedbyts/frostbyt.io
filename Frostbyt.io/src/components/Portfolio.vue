4a9dd7bea0d1b642a4b48c0c354fac6b515a4e96
turnerlehm <edmfrosty@gmail.com>
2020-11-07 14:48:23 -0800

    Adding my gamejam game to the portfolio

--- Frostbyt.io/src/components/Portfolio.vue
+++ Frostbyt.io/src/components/Portfolio.vue
@@ -21,7 +21,7 @@
                     </div>
                     <div class="card-body">
                         <div class="text-center card-title">
-                            <img alt="Vue Logo" class="rounded-circle img-thumb" src="@/assets/images/vanguard_api.png" />
+                            <img alt="Threat Intel API" class="rounded-circle img-thumb" src="@/assets/images/vanguard_api.png" />
                         </div>
                         <div class="text-center card-title">
                             <h4>Threat Intel API and Feed Aggregator</h4>
@@ -35,6 +35,30 @@
                         </div>
                         <div class="text-center">
                             <a href="https://vanguard.hyvemined.com" class="btn btn-secondary">Live Site</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card-body">
+                        <div class="text-center card-title">
+                            <img alt="Now Run Game" class="rounded-circle img-thumb" src="@/assets/images/now_run.png"/>
+                        </div>
+                        <div class="text-center card-title">
+                            <h4>Now Run - a MOONJAM 2020 Game</h4>
+                        </div>
+                        <div class="text-center">
+                            <p>
+                                This is a basic shoot-em-up dungeon crawler/rogue-ish like game I made in the Godot engine for the 2020 Mooncord game jam (sponsored by Twitch streamer MOONMOON) with a loose horror theme. 
+                                All dungeons and enemies are procedurally generated at the start of each run and the goal of the game is to get from the start room to the end room without dying.
+                                You can either shoot or run your way to the end. There are some bugs since this game was developed in about 72 hours (3 days) and in engine I had never used before and with little artistic skill.
+                                I do plan on continuing development of this at some point into a more refined game and releasing it on Itch.io.
+                                The main reasons I chose the Godot engine as opposed to something like unity or Unreal is that the Godot engine is 100% open-source, something I am very passionate about as I prefer to use OSS.
+                                Additionally Godot's scripting language is very similar to Python and was easy to pick up and understand with the help of some tutorials. Source code and a link to the gamejame site can be found below.
+                            </p>
+                        </div>
+                        <div class="text-center">
+                            <a href="https://moonjam.vercel.app/games" class="btn btn-secondary mr-1">Gamejam Site</a>
+                            <a href="https://github.com/frostedbyts/NowRun" class="btn btn-secondary">Source Code</a>
                         </div>
                     </div>
                 </div>
