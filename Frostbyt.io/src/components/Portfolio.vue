b530a862927e05b0048245e6913397babf3e1c0b
turnerlehm <edmfrosty@gmail.com>
2020-11-11 07:14:51 -0800

    Fixing some typos. removing live link to threat intel site

--- Frostbyt.io/src/components/Portfolio.vue
+++ Frostbyt.io/src/components/Portfolio.vue
@@ -30,11 +30,8 @@
                             <p>
                                 This is a custom threat intelligence API and feed aggregator I built in C# using .NET Core 3.1. This API/aggregator aggregates or pulls from almost 30 different sources to aggregate and index both directly
                                 and indirectly hundreds of different threat intelligence feeds and exposes them through a simple, standard API with relatively fast querying abilities (within seconds). The database portion of the API is powered by Entity Framework Core and at last count was indexing almost 200,000+
-                                unique threat intelligence reports. The UI/documentation was built using SwaggerUI via the Swashbuckle library for .NET Core.
+                                unique threat intelligence reports. The UI/documentation was built using SwaggerUI via the Swashbuckle library for .NET Core. Unfortunately, due to security and copyright concerns, I am unable to provide a live link to this site except to those who work at my employer
                             </p>
-                        </div>
-                        <div class="text-center">
-                            <a href="https://vanguard.hyvemined.com" class="btn btn-secondary">Live Site</a>
                         </div>
                     </div>
                 </div>
@@ -50,10 +47,10 @@
                             <p>
                                 This is a basic shoot-em-up dungeon crawler/rogue-ish like game I made in the Godot engine for the 2020 Mooncord game jam (sponsored by Twitch streamer MOONMOON) with a loose horror theme. 
                                 All dungeons and enemies are procedurally generated at the start of each run and the goal of the game is to get from the start room to the end room without dying.
-                                You can either shoot or run your way to the end. There are some bugs since this game was developed in about 72 hours (3 days) and in engine I had never used before and with little artistic skill.
+                                You can either shoot or run your way to the end. There are some bugs since this game was developed in about 72 hours (3 days) and in an engine I had never used before and with little artistic skill.
                                 I do plan on continuing development of this at some point into a more refined game and releasing it on Itch.io.
-                                The main reasons I chose the Godot engine as opposed to something like unity or Unreal is that the Godot engine is 100% open-source, something I am very passionate about as I prefer to use OSS.
-                                Additionally Godot's scripting language is very similar to Python and was easy to pick up and understand with the help of some tutorials. Source code and a link to the gamejame site can be found below.
+                                The main reasons I chose the Godot engine as opposed to something like Unity or Unreal is that the Godot engine is 100% open-source, something I am very passionate about as I prefer to use OSS.
+                                Additionally Godot's scripting language is very similar to Python and was easy to pick up and understand with the help of some tutorials. Source code and a link to the gamejam site can be found below.
                             </p>
                         </div>
                         <div class="text-center">
